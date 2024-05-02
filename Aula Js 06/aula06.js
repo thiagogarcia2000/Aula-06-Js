705266f6d3476b38e7d97dd394d6c9e5cb81ddbb
thiagogarcia2000 <158522653+thiagogarcia2000@users.noreply.github.com>
2024-05-02 18:47:55 -0300

    Add files via upload

---
+++ Aula Js 06/aula06.js
@@ -0,0 +1,97 @@
+// let lista_digitado = []
+// let lista_dobro = []
+
+// for( let i =0; i<10; i++) {
+//     const numero_digitado = Number(prompt("Digite um numero"))
+    // lista_digitado.push(numero_digitado);
+//     lista_dobro.push(numero_digitado*2)
+// }
+
+    // console.log(lista_digitado);
+//     console.log(lista_dobro);
+
+// let lista_digitado = []
+// let soma = 0
+
+// for(let i = 0; i<8; i++){
+// const numero_digitado = Number(prompt("Digite um numero"))
+// lista_digitado.push(numero_digitado)
+// }
+
+// console.log(lista_digitado);
+
+// for(let numero of lista_digitado){
+// if(numero % 2 === 0){
+//     soma += numero
+// }
+// }
+// console.log(`A soma dos numeros pares é igual a ${soma}`);
+
+// let lista_digitado = []
+// let lista2 = []
+
+// for(let i = 0; i<6; i++){
+// const numero_digitado = Number(prompt("Digite um numero"))
+// lista_digitado.push(numero_digitado)
+// }
+
+// console.log(lista_digitado);
+
+// for(let numero of lista_digitado){
+// if(numero % 3 === 0 && numero % 5 ===0)
+// lista2.push(numero)
+// }
+// console.log(lista2);
+
+// function somar(primeiro, segundo) {
+//     const soma = primeiro + segundo
+//     console.log(`A soma dos numeros é ${soma}`);    
+// }
+
+// somar(8,10)
+// somar(20,15)
+// somar(200,30)
+
+// function contarVogais(palavra){
+//     const vogais = "aeiouáàâãéêíõóôú"
+//     let contador_de_vogais = 0
+//     for(let letra_da_vez of palavra){
+//         if(vogais.includes(letra_da_vez)){
+//             contador_de_vogais ++
+//         }
+//     }
+//     return contador_de_vogais
+// }
+
+// const palavra_digitada = prompt("Digite uma palavra")
+// console.log(contarVogais(palavra_digitada))
+// console.log(contarVogais("beterraba"))
+// console.log(contarVogais("melancia"))
+// console.log(contarVogais("abel")) 
+
+// function quadrado(numero){
+//     return numero ** 2
+// }
+
+// console.log(quadrado(5))
+// console.log(quadrado(60))
+// console.log(quadrado(14))
+
+// const numero = Number(prompt("Digite um número: "))
+// console.log(quadrado(numero))
+
+
+// const numero_digitado = Number(prompt("Digite um número: "))
+// console.log(quadrado(numero_digitado))
+
+// function calcularCirculo(r){
+//     const area = Math.PI * r ** 2 
+//     const circunferencia = 2 * Math.PI * r 
+
+//     return [area.toFixed(2), circunferencia.toFixed(2)]
+// }
+
+// const raio = Number(prompt("Digite o raio do circulo: "))
+// const circulo1 = calcularCirculo(raio)
+// console.log(`Área do circulo ${circulo1[0]}`)
+// console.log(`Circunferência do circulo ${circulo1[1]}`)
